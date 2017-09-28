9ad3dfcc9fe495f7ca152dbb59fc0ed6d7a77ea0
duncanrudd <dunc@3drstudio.co.uk>
2017-09-28 22:59:05 +0100

    snake wip

--- components/snake.py
+++ components/snake.py
@@ -14,7 +14,6 @@ import math
 
 class DrSnake(drBase.DrBaseComponent):
     '''
-
     '''
     def __init__(self, start, end, name, numFkCtrls, jointsPerCtrl, numIkCtrls, latticeDivisions=4, axis='x', upAxis='y',  cleanUp=1):
         super(DrSnake, self).__init__(name)
@@ -22,7 +21,8 @@ class DrSnake(drBase.DrBaseComponent):
         self.build(start, end, numFkCtrls, jointsPerCtrl, numIkCtrls, latticeDivisions, axis, upAxis)
 
         if cleanUp:
-            self.cleanUp()
+            # self.cleanUp()
+            pass
 
     def build(self, start, end, numFkCtrls, jointsPerCtrl, numIkCtrls, latticeDivisions, axis, upAxis):
         start, end = coreUtils.getStartAndEnd(start, end)
@@ -43,6 +43,7 @@ class DrSnake(drBase.DrBaseComponent):
 
         # ik ctrls
         ikCtrls=[]
+        refVecs = []
         for i in range(len(points)):
             num = str(i+1).zfill(2)
 
@@ -51,18 +52,31 @@ class DrSnake(drBase.DrBaseComponent):
             b.setParent(self.interfaceGrp)
             b.t.set(points[i])
             ikCtrls.append(c)
+            refVecs.append(coreUtils.matrixAxisToVector(c, name='%s_referenceVec_%s_utl' % (self.name, num), axis='x'))
         ikCtrls[1].getParent().setParent(ikCtrls[0])
         ikCtrls[-2].getParent().setParent(ikCtrls[-1])
 
+
         # tangent vectors
         tangentVecs = []
+        tangentVecs.append(coreUtils.matrixAxisToVector(ikCtrls[0], name='%s_tangentVec_00_utl' % self.name, axis='z'))
         for i in range(len(ikCtrls)-1):
             num = str(i+1).zfill(2)
             vec = coreUtils.vectorBetweenNodes(ikCtrls[i], ikCtrls[i+1], name='%s_tangentVec_%s_utl' % (self.name, num))
             vecNorm = coreUtils.normalizeVector(vec.output3D, name='%s_tangentVecNorm_%s_utl' % (self.name, num))
             tangentVecs.append(vecNorm)
 
-        tangentVecs.append(coreUtils.matrixAxisToVector(ikCtrls[-1], name='%s_tangentVec_%s_utl' % (self.name, str(len(points)).zfill(2))))
+        tangentVecs.append(coreUtils.matrixAxisToVector(ikCtrls[-1], name='%s_tangentVec_%s_utl' % (self.name, str(len(points)).zfill(2)), axis='z'))
+
+        blendedTangentVecs=[]
+        # create blended tangent vector
+        for i in range(len(ikCtrls)):
+            num = str(i+1).zfill(2)
+            bc = coreUtils.blend(tangentVecs[i].output, tangentVecs[i+1].output, name='%s_blendedTangentVec_%s_utl' % (self.name, num))
+            bc.blender.set(0.5)
+            blendedTangentVecNorm = coreUtils.normalizeVector(bc.output, name='%s_blendedTangentVecNorm_%s_utl' % (self.name, num))
+            blendedTangentVecs.append(blendedTangentVecNorm)
+        blendedTangentVecs.append(tangentVecs[-1])
 
         # create sharp matrices at each lattice division
         for i in range(len(ikCtrls)-1):
@@ -73,9 +87,8 @@ class DrSnake(drBase.DrBaseComponent):
                 divs = latticeDivisions
             segMtx = pmc.createNode('fourByFourMatrix', name='%s_segMtx_%s_utl' % (self.name, num))
 
-            refVec = coreUtils.matrixAxisToVector(ikCtrls[i], name='%s_referenceVec_%s_utl' % (self.name, num), axis='x')
-            upVec = coreUtils.cross(tangentVecs[i].output, refVec.output, name='%s_upVec_%s_utl' % (self.name, num))
-            sideVec = coreUtils.cross(upVec.output, tangentVecs[i].output, name='%s_sideVec_%s_utl' % (self.name, num))
+            upVec = coreUtils.cross(tangentVecs[i+1].output, refVecs[i].output, name='%s_upVec_%s_utl' % (self.name, num))
+            sideVec = coreUtils.cross(upVec.output, tangentVecs[i+1].output, name='%s_sideVec_%s_utl' % (self.name, num))
 
             sideVec.outputX.connect(segMtx.in00)
             sideVec.outputY.connect(segMtx.in01)
@@ -85,9 +98,9 @@ class DrSnake(drBase.DrBaseComponent):
             upVec.outputY.connect(segMtx.in11)
             upVec.outputZ.connect(segMtx.in12)
 
-            tangentVecs[i].outputX.connect(segMtx.in20)
-            tangentVecs[i].outputY.connect(segMtx.in21)
-            tangentVecs[i].outputZ.connect(segMtx.in22)
+            tangentVecs[i+1].outputX.connect(segMtx.in20)
+            tangentVecs[i+1].outputY.connect(segMtx.in21)
+            tangentVecs[i+1].outputZ.connect(segMtx.in22)
 
             d = coreUtils.decomposeMatrix(ikCtrls[i].worldMatrix[0], name='%s_segMtxToSrt_%s_utl' % (self.name, num))
             d.outputTranslateX.connect(segMtx.in30)
@@ -99,18 +112,6 @@ class DrSnake(drBase.DrBaseComponent):
             for a in range(divs):
                 num = str((i*divs)+a+1).zfill(2)
                 blendVal = (1.0 / (divs))*a
-
-                grp = pmc.group(empty=1, name='%s_pointBase_%s_srt' % (self.name, num))
-                grp.setParent(self.rigGrp)
-
-                # Create matrix for base group
-                '''
-                yVec = coreUtils.matrixAxisToVector(localMtx.matrixSum, name='%s_pointYVec_%s_utl' % (self.name, num), axis='y')
-                scaleY = coreUtils.divide(1.0, yVec.outputY, name='%s_pointScaleY_%s_utl' % (self.name, num))
-
-                xVec = coreUtils.matrixAxisToVector(localMtx.matrixSum, name='%s_pointXVec_%s_utl' % (self.name, num), axis='x')
-                scaleX = coreUtils.divide(1.0, xVec.outputX, name='%s_pointScaleX_%s_utl' % (self.name, num))
-                '''
 
                 mtx = None
                 d = None
@@ -124,78 +125,53 @@ class DrSnake(drBase.DrBaseComponent):
                     mtx = coreUtils.multiplyMatrices([localMtx.output, segMtx.output], name='%s_pointMtx_%s_utl' % (self.name, num))
                     d = coreUtils.decomposeMatrix(mtx.matrixSum, '%s_pointMtxToSrt_%s_utl' % (self.name, num))
 
-                coreUtils.connectDecomposedMatrix(d, grp)
+                # Create a locator the orients according to blended tangents
+                loc = coreUtils.addChild(self.rigGrp, 'locator', name='%s_pointOrient_%s_srt' % (self.name, num))
 
+                pointFrontVec = coreUtils.blend(blendedTangentVecs[i+1].output, blendedTangentVecs[i].output, name='%s_pointFrontVec_%s_utl' % (self.name, num))
+                pointFrontVec.blender.set(blendVal)
+                pointFrontVecNorm = coreUtils.normalizeVector(pointFrontVec.output, name='%s_pointFrontVecNorm_%s_utl' % (self.name, num))
 
+                pointRefVec = coreUtils.blend(refVecs[i+1].output, refVecs[i].output, name='%s_pointRefVec_%s_utl' % (self.name, num))
+                pointRefVec.blender.set(blendVal)
+                pointRefVecNorm = coreUtils.normalizeVector(pointRefVec.output, name='%s_pointReferenceVecNorm_%s_utl' % (self.name, num))
 
+                pointUpVec = coreUtils.cross(pointFrontVecNorm.output, pointRefVecNorm.output, name='%s_pointUpVec_%s_utl' % (self.name, num))
+                pointSideVec = coreUtils.cross(pointUpVec.output, pointFrontVecNorm.output, name='%s_pointUpVec_%s_utl' % (self.name, num))
 
+                pointMtx = pmc.createNode('fourByFourMatrix', name='%s_pointOrientMtx_%s_utl' % (self.name, num))
 
+                pointSideVec.outputX.connect(pointMtx.in00)
+                pointSideVec.outputY.connect(pointMtx.in01)
+                pointSideVec.outputZ.connect(pointMtx.in02)
 
+                pointUpVec.outputX.connect(pointMtx.in10)
+                pointUpVec.outputY.connect(pointMtx.in11)
+                pointUpVec.outputZ.connect(pointMtx.in12)
 
+                pointFrontVec.outputR.connect(pointMtx.in20)
+                pointFrontVec.outputG.connect(pointMtx.in21)
+                pointFrontVec.outputB.connect(pointMtx.in22)
 
+                d.outputTranslateX.connect(pointMtx.in30)
+                d.outputTranslateY.connect(pointMtx.in31)
+                d.outputTranslateZ.connect(pointMtx.in32)
 
+                parentMtx = None
+                if a == 0:
+                    parentMtx = coreUtils.inverseMatrix(mtx.output, name='%s_parentInverseMtx_%s_utl' % (self.name, num))
+                else:
+                    parentMtx = coreUtils.inverseMatrix(mtx.matrixSum, name='%s_parentInverseMtx_%s_utl' % (self.name, num))
+                localOrientMtx = coreUtils.multiplyMatrices([pointMtx.output, parentMtx.outputMatrix], name='%s_pointMtx_%s_utl' % (self.name, num))
 
+                yVec = coreUtils.matrixAxisToVector(localOrientMtx.matrixSum, name='%s_pointYVec_%s_utl' % (self.name, num), axis='y')
+                scaleY = coreUtils.divide(1.0, yVec.outputY, name='%s_pointScaleY_%s_utl' % (self.name, num))
 
-        '''
-        posYPointsSharp = []
-        negYPointsSharp = []
-        posXPointsSharp = []
-        negXPointsSharp = []
+                xVec = coreUtils.matrixAxisToVector(localOrientMtx.matrixSum, name='%s_pointXVec_%s_utl' % (self.name, num), axis='x')
+                scaleX = coreUtils.divide(1.0, xVec.outputX, name='%s_pointScaleX_%s_utl' % (self.name, num))
 
-        posYPointsSharp.append(coreUtils.pointMatrixMult((0, 1, 0), ikCtrls[0].worldMatrix[0], name='%s_posY_sharpPoint_%s_utl' % (self.name, num)))
-        negYPointsSharp.append(coreUtils.pointMatrixMult((0, -1, 0), ikCtrls[0].worldMatrix[0], name='%s_negY_sharpPoint_%s_utl' % (self.name, num)))
-        posXPointsSharp.append(coreUtils.pointMatrixMult((1, 0, 0), ikCtrls[0].worldMatrix[0], name='%s_posX_sharpPoint_%s_utl' % (self.name, num)))
-        negXPointsSharp.append(coreUtils.pointMatrixMult((-1, 0, 0), ikCtrls[0].worldMatrix[0], name='%s_negX_sharpPoint_%s_utl' % (self.name, num)))
-
-        for i in range(len(jointMtxList)-1):
-            divs = latticeDivisions*2
-            exceptionList = [0, 1, len(jointMtxList)-2, len(jointMtxList)-3]
-            if i in exceptionList:
-                divs = latticeDivisions
-            for a in range(divs):
-                num = str((i*divs)+a).zfill(2)
-                blendVal = (1.0 / (divs))*a
-
-                mtxBlend = coreUtils.blendMatrices(jointMtxList[i].output, jointMtxList[i+1].output, 1-blendVal, blendVal, name='%s_mtxBlend_%s_utl' % (self.name, num))
-
-                posYPointsSharp.append(coreUtils.pointMatrixMult((0, 1, 0), mtxBlend.matrixSum, name='%s_posY_sharpPoint_%s_utl' % (self.name, num)))
-                negYPointsSharp.append(coreUtils.pointMatrixMult((0, -1, 0), mtxBlend.matrixSum, name='%s_negY_sharpPoint_%s_utl' % (self.name, num)))
-                posXPointsSharp.append(coreUtils.pointMatrixMult((1, 0, 0), mtxBlend.matrixSum, name='%s_posX_sharpPoint_%s_utl' % (self.name, num)))
-                negXPointsSharp.append(coreUtils.pointMatrixMult((-1, 0, 0), mtxBlend.matrixSum, name='%s_negX_sharpPoint_%s_utl' % (self.name, num)))
-
-        posYPointsSharp.append(coreUtils.pointMatrixMult((0, 1, 0), ikCtrls[-1].worldMatrix[0], name='%s_posY_sharpPoint_%s_utl' % (self.name, num)))
-        negYPointsSharp.append(coreUtils.pointMatrixMult((0, -1, 0), ikCtrls[-1].worldMatrix[0], name='%s_negY_sharpPoint_%s_utl' % (self.name, num)))
-        posXPointsSharp.append(coreUtils.pointMatrixMult((1, 0, 0), ikCtrls[-1].worldMatrix[0], name='%s_posX_sharpPoint_%s_utl' % (self.name, num)))
-        negXPointsSharp.append(coreUtils.pointMatrixMult((-1, 0, 0), ikCtrls[-1].worldMatrix[0], name='%s_negX_sharpPoint_%s_utl' % (self.name, num)))
-
-        # create lattice
-        latticeScale = math.sqrt(1*2)
-        pmc.select([])
-        lattice = pmc.lattice(dv=(2, 2, len(posYPointsSharp)), scale=(1, 1, 1))
-        lattice[0].rename('%s_lattice_def' % self.name)
-        lattice[1].rename('%s_lattice_cage' % self.name)
-        lattice[2].rename('%s_lattice_base' % self.name)
-        lattice[2].rz.set(45)
-        lattice[2].s.set((latticeScale, latticeScale, coreUtils.getDistance(start, end)))
-
-
-        for i in range(len(posYPointsSharp)):
-            posYPointsSharp[i].output.connect(lattice[1].controlPoints[(i*4)+3])
-            negYPointsSharp[i].output.connect(lattice[1].controlPoints[i*4])
-            posXPointsSharp[i].output.connect(lattice[1].controlPoints[(i*4)+1])
-            negXPointsSharp[i].output.connect(lattice[1].controlPoints[(i*4)+2])
-        '''
-
-
-
-
-
-
-
-
-
-
-    def cleanUp(self):
-        pass
-        #super(DrSnake, self).cleanUp()
-
+                d = coreUtils.decomposeMatrix(pointMtx.output, name='%s_pointMtxToSrt_%s_utl' % (self.name, num))
+                d.outputTranslate.connect(loc.t)
+                d.outputRotate.connect(loc.r)
+                scaleY.outputX.connect(loc.sy)
+                scaleX.outputX.connect(loc.sx)
